88df6e6441c9bfa01ae02a02f14c73778937d856
Unknown <nordban@gmail.com>
2018-01-18 19:32:33 -0700

    updating

---
+++ DGM-1600-Spring-Game/Assets/Scripts/PlayerOneMovment.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerOneMovment : MonoBehaviour {
+
+    public float transX;
+    public float transY;
+    public float transZ;
+
+    public float rotX;
+    public float rotY;
+    public float rotZ;
+
+    public string firstName = "";
+
+    private double cheeseBurger = 0.5;
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate ( transX, transY, transZ);
+        transform.Rotate(rotX, rotY, rotZ);
+
+    }
+}
